7aad0dd9090b95c5b31d6b1bd56bb023a13768a9
Tucker Miles <tmiles7@vols.utk.edu>
2021-02-14 21:13:17 -0500

    Painter for UI done

--- attendio/lib/login.dart
+++ attendio/lib/login.dart
@@ -12,8 +12,8 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.white,
+      body: CustomPaint(
+        painter: BluePainter(), //FF is transparency
         child: Center(
           child: Column(
             mainAxisSize: MainAxisSize.max,
@@ -69,5 +69,39 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
+  }
+}
+
+// https://www.youtube.com/watch?v=u96GgqHFy3c
+class BluePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final height = size.height;
+    final width = size.width;
+    Paint paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, width, height));
+    paint.color = Color(0xFF6A1B9A);
+    canvas.drawPath(mainBackground, paint);
+
+    final p1 = Offset(0, height * 2);
+    final p2 = Offset(width * 1.5, height * 0.5);
+    Paint paint2 = Paint()
+      ..color = Color(0xFFB39DDB)
+      ..strokeWidth = 400;
+    canvas.drawLine(p1, p2, paint2);
+
+    final p3 = Offset(-150, height * 0.8);
+    final p4 = Offset(width, height * 1.2);
+    Paint paint3 = Paint()
+      ..color = Color(0xFFFFFFFF)
+      ..strokeWidth = 400;
+    canvas.drawLine(p3, p4, paint3);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return oldDelegate != this;
   }
 }
