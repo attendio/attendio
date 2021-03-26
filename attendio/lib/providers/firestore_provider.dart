40f53682bd90ea762a28b24ea0ac0d683f800bee
daniel-troutman <dtroutm1@vols.utk.edu>
2021-03-26 09:18:52 -0400

    Add firestore provider #54

---
+++ attendio/lib/providers/firestore_provider.dart
@@ -0,0 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final firestoreProvider =
+    Provider<FirebaseFirestore>((ref) => FirebaseFirestore.instance);
