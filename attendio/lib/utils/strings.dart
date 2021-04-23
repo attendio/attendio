5fd05bb3b690d347364d6172dcd4c1fd5592b788
Spencer Howell <spencerhowell98@gmail.com>
2021-04-23 00:23:23 -0400

    Add Strings file to store all static string references

---
+++ attendio/lib/utils/strings.dart
@@ -0,0 +1,9 @@
+class Strings {
+
+  // Take Attendance screen strings
+  static String signInTo = "Sign in to";
+  static String signInSubtitle = "We are so glad you're here!";
+  static String instructionsTitle = "Instructions:";
+  static String instructionsBodyText1 = "Use your phone to scan the above QR code.";
+  static String instructionsBodyText2 = "If you haven't installed Attendio, follow the onscreen instructions";
+}
