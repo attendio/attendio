b9f0e71fd6581d27ea66776a3cf6d031d12e7ec2
Tucker Miles <tmiles7@vols.utk.edu>
2021-04-25 11:56:12 -0400

    Navbar working

--- attendio/lib/main.dart
+++ attendio/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:attendio/pages/create_event.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
